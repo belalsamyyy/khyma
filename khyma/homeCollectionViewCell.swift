6f08cf14b77553c7afbabe5ddd25a652572b69b1
alyHwaya <alyappleid4business@gmail.com>
2021-09-12 10:32:18 +0200

    first

---
+++ khyma/homeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  homeCollectionViewCell.swift
+//  khyma
+//
+//  Created by aly hassan on 11/09/2021.
+//
+
+import UIKit
+
+class homeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var image: UIImageView!
+}
